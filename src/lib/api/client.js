911e437d4897b377d56b5a7215df41a612d6a5d1
tinodevclumsy <seungjun.dev@gmail.com>
2023-11-28 17:18:16 -0800

    updated axios

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const client = axios.create();
 
+client.defaults.withCredentials = true;
+
 client.defaults.baseURL = 'https://wordzzle-backend-tinodevclumsy.koyeb.app/api'
 
 export default client;
